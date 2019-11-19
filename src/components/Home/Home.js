1faf1803aad1d189f9a138b83caff830f60bbcb7
André Luís Bordignon <albordignon@gmail.com>
2019-11-19 09:59:41 -0300

    Versão inicial do software. Criada a página Home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const home = () => {
+    return(
+        <div className='Home'>
+            <h1>Teste</h1>
+        </div>
+    );
+}
+
+export default home;
+
