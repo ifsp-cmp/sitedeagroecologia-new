6aba179c29d79d8e9e08728e7c92e7542aef27c1
André Luís Bordignon <albordignon@gmail.com>
2019-11-22 11:23:14 -0300

    Criada a estrutura de pastas de assets

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Home.css';
+import foto from './foto1.jpg';
 
 const home = () => {
     return(
         <div className='Home'>
            <p>AGROECOLOGIA EM REDE</p>
-           <img id="img1" href="foto1.jpg"></img>
+           <img id="img1" src={foto}></img>
         </div>
     );
 }
