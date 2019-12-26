518e800aeabdb2e901f17cbb4f12137ebddb8697
André Luís Bordignon <albordignon@gmail.com>
2019-12-26 11:32:17 -0300

    Acerto do menu dropdown

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
 import NavigationItem from './NavigationItem/NavigationItem';
+import { NavLink } from 'react-router-dom';
+
+import arrow from '../../../assets/images/menu/arrow.svg';
 
 import './NavigationItems.css'
 
 const navigationItems = (props) => (
     <ul className='NavigationItems'>
         <NavigationItem link='/' active>Início</NavigationItem>
-        <NavigationItem link='/politicasnacionais'>Políticas Nacionais</NavigationItem>
+
+        <li className='Menu1'>Políticas Nacionais New <img src={arrow} />
+            <ul className='SubMenu1'>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </li>
+
+        <NavigationItem className='Menu1' link='/politicasnacionais'>Políticas Nacionais <img src={arrow} /></NavigationItem>
         <NavigationItem link='/assentamentoms'>Assentamento Milton Santos</NavigationItem>
         <NavigationItem link='/tecnologiassociais'>Tecnologias Sociais</NavigationItem>
         <NavigationItem link='/educacaopopular'>Educação Popular</NavigationItem>
         <NavigationItem link='/quemsomos'>Quem Somos</NavigationItem>
 
-        { props.isAuthenticated ?
+        {props.isAuthenticated ?
             <NavigationItem link='/training'>Formação</NavigationItem> : null
         }
-        { !props.isAuthenticated ?
-            <NavigationItem link='/cadastro'>Cadastro</NavigationItem> :  null
+        {!props.isAuthenticated ?
+            <NavigationItem link='/cadastro'>Cadastro</NavigationItem> : null
         }
-        { !props.isAuthenticated 
+        {!props.isAuthenticated
             ? <NavigationItem link='/login'>Login</NavigationItem>
             : <NavigationItem link='/logout'>Logout</NavigationItem>
         }
