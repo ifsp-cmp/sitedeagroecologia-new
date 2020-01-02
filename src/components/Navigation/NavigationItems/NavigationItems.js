0ebd5842081e20ae7becb78bf72d8254454340d4
André Luís Bordignon <albordignon@gmail.com>
2020-01-02 16:41:36 -0300

    Finalizado Menu

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -11,14 +11,55 @@ const navigationItems = (props) => (
     <ul className='NavigationItems'>
         <NavigationItem link='/' active>Início</NavigationItem>
 
-        <li className='Menu1'>Políticas Nacionais New <img src={arrow} />
+        <li className='Menu1'>
+            <p className="TituloMenu">Políticas Nacionais <img src={arrow} /></p>
             <ul className='SubMenu1'>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                <NavigationItem link='/pnapo' active>PNAPO</NavigationItem>
+                <NavigationItem link='/planapo' active>PLANAPO</NavigationItem>
+                <NavigationItem link='/plansan' active>PLANSAN</NavigationItem>
+                <NavigationItem link='/paa' active>PAA</NavigationItem>
+                <NavigationItem link='/pnae' active>PNAE</NavigationItem>
             </ul>
         </li>
 
-        <NavigationItem className='Menu1' link='/politicasnacionais'>Políticas Nacionais <img src={arrow} /></NavigationItem>
+        <li className='Menu1'>
+            <p className="TituloMenu">Assentamento Milton Santos <img src={arrow} /></p>
+            <ul className='SubMenu1'>
+                <NavigationItem link='/mslutassociais' active>Lutas Sociais</NavigationItem>
+                <NavigationItem link='/mscooperflora' active>Cooperativa Cooperflora</NavigationItem>
+                <NavigationItem link='/msgruposdeconsumo' active>Grupos de Consumo</NavigationItem>
+                <NavigationItem link='/msfeiras' active>Feiras</NavigationItem>
+
+            </ul>
+        </li>
+
+        <li className='Menu1'>
+            <p className="TituloMenu">Tecnologias Sociais <img src={arrow} /></p>
+            <ul className='SubMenu1'>
+                <NavigationItem link='/tsagrocomunicacao' active>Agrocomunicação</NavigationItem>
+                <NavigationItem link='/tsdatalutaproducao' active>Dataluta Produção</NavigationItem>
+                <NavigationItem link='/tsirrigador' active>Irrigador</NavigationItem>
+                <NavigationItem link='/tsminhocarioautomatizado' active>Minhocário Automatizado</NavigationItem>
+                <NavigationItem link='/rsmapeamentodaproducao' active>Mapeamento da Produção</NavigationItem>
+                <NavigationItem link='/tsfichasagroecologicas' active>Fichas Agroecológicas</NavigationItem>
+            </ul>
+        </li>
+
+        <li className='Menu1'>
+            <p className="TituloMenu">Educação Popular <img src={arrow} /></p>
+            <ul className='SubMenu1'>
+                <NavigationItem link='/epcursosfic' active>Cursos Fic</NavigationItem>
+                <NavigationItem link='/epformacoes' active>Formações</NavigationItem>
+                <NavigationItem link='/epdiagnosticoruralparticipativo' active>Diagnóstico Rural Participativo</NavigationItem>
+                <NavigationItem link='/avaliacoesevalidacoesparticipativas' active>Avaliações e Validações Participativas</NavigationItem>
+                <NavigationItem link='/epvisitasevivencias' active>Visitas e Vivências</NavigationItem>
+            </ul>
+        </li>
+
+        <NavigationItem link='/quemsomos'>Quem Somos</NavigationItem>
+        
+
+        {/* <NavigationItem className='Menu1' link='/politicasnacionais'>Políticas Nacionais <img src={arrow} /></NavigationItem>
         <NavigationItem link='/assentamentoms'>Assentamento Milton Santos</NavigationItem>
         <NavigationItem link='/tecnologiassociais'>Tecnologias Sociais</NavigationItem>
         <NavigationItem link='/educacaopopular'>Educação Popular</NavigationItem>
@@ -33,7 +74,7 @@ const navigationItems = (props) => (
         {!props.isAuthenticated
             ? <NavigationItem link='/login'>Login</NavigationItem>
             : <NavigationItem link='/logout'>Logout</NavigationItem>
-        }
+        } */}
     </ul>
 );
 
