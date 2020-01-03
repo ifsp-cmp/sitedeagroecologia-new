baa224962aaba5029b7da4f06351e66ac0de9997
André Luís Bordignon <albordignon@gmail.com>
2020-01-03 14:44:07 -0300

    Ajustando o rodapé

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -8,18 +8,77 @@ import license from '../../../assets/images/license.png';
 import logoIfspCampinas from '../../../assets/images/logoifspcampinas.png';
 import logoNeaes from '../../../assets/images/logoneaes.png';
 
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
+
+import phoneIcon from '../../../assets/images/icons/phoneicon.svg';
+import emailIcon from '../../../assets/images/icons/emailicon.svg';
+
+
 const footer = () => {
     return (
-        <footer className="Footer Bloco">
-            <div className="AcessoRapido">
+        <footer className="Footer">
+            <div className="AcessoRapido Bloco">
                 <p className='Titulo'>Acesso Rápido</p>
+                <NavLink to='/' exact>Início</NavLink>
+                <br />
+                <NavLink to='/quemsomos' exact>Quem Somos</NavLink>
             </div>
             <div className='PaginasdeInteresse Bloco'>
                 <p className='Titulo'>Páginas de Interesse</p>
+                <li><a href="http://agroecologia.gov.br/">Agroecologia Governo Federal</a></li>
+                <li><a href="http://www.osala-agroecologia.org/">Observatório de Soberania Alimentar e Agroecologia</a></li>
+                <li><a href="http://aba-agroecologia.org.br/">Associação Brasileira de Agroecologia</a></li>
+                <li><a href="https://www.embrapa.br/">Embrapa</a></li>
+                <li><a href="http://www.cati.sp.gov.br/">Coordenaria de Desenvolvimento Rural Sustentável</a></li>
+                <li><a href="http://www.incra.gov.br/">INCRA</a></li>
 
             </div>
-            <div className='Contato'>
+            <div className='Contato Bloco'>
                 <p className='Titulo'>Entre em Contato</p>
+                <div>
+                    <p> Av. Comendador Aladino Selmi, s/n,
+                        <br />Bairro Amarais
+                        <br />Campinas - SP
+                        <br />CEP: 13069-901
+                    </p>
+                    <div className='Fone'>
+                        <div className='Icone'>
+                            <img src={phoneIcon} alt='Phone Icon'></img>
+                        </div>
+                        (19) 3746-6128
+                    </div>
+                    <div className='Email'>
+                        <div className='Icone'>
+                            <img src={emailIcon} alt='Email Icon'></img>
+                        </div>
+                        erika.batista@ifsp.edu.br
+                    </div>
+                    <div className='Facebook'>
+                        <p>Acompanhe as novidades</p>
+                        <a href="https://www.facebook.com/neaes.ifsp.campinas/">
+                            <div className='IconeFacebook Icone'>
+                                <spam>f</spam>
+                            </div>
+                        </a>
+                    </div>
+
+                    {/* <dt>Facebook</dt>
+                                <dd><a href="https://www.facebook.com/neaes.ifsp.campinas/">https://www.facebook.com/neaes.ifsp.campinas/</a></dd>
+                                <dt>Email</dt>
+                                <dd><a href="mailto:erika.batista@ifsp.edu.br">erika.batista@ifsp.edu.br</a></dd>
+                                <dt>Endereço</dt>
+                                <dd>
+                                    Av. Comendador Aladino Selmi, s/n, <br />
+                                    Bairro Amarais<br />
+                                    CEP: 13069-901<br />
+                                    Campinas - SP
+											</dd>
+                                <dt>Telefone</dt>
+                                <dd>(19) 3746-6128</dd>
+                            </dl>
+                        </div>
+                    </div> */}
+                </div>
 
             </div>
             {/* <div className="Bloco">
