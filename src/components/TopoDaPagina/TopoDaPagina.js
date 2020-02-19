c15c8e5c15b11db591967b26c416ca17a21e0bad
joaodias <joaovitordias92040@gmail.com>
2020-02-19 10:16:30 -0300

    Arrumando o espaço do caminho

--- src/components/TopoDaPagina/TopoDaPagina.js
+++ src/components/TopoDaPagina/TopoDaPagina.js
@@ -4,7 +4,7 @@ import './TopoDaPagina.css';
 
 const topoDaPagina = (props) => {
 
-    const caminho = props.submenu ? <span>{props.menu}>{props.submenu}</span> : <span>{props.menu}</span>;
+    const caminho = props.submenu ? <span>{props.menu} > {props.submenu}</span> : <span>{props.menu}</span>;
 
     return (
         <div className="TopoDaPagina">
