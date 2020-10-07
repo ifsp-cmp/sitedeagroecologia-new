a2c8e53c5b7c93551c5a0cabc0db319b4ba56753
João <joaovitordias92040@gmail.com>
2020-10-07 18:13:15 -0300

    Arrumado a pag Agrocomunicacao

--- src/components/TecnologiasSociais/AppAgroecomunicacao/AppAgroecomunicacao.js
+++ src/components/TecnologiasSociais/AppAgroecomunicacao/AppAgroecomunicacao.js
@@ -3,7 +3,7 @@ import TopoDaPagina from '../../TopoDaPagina/TopoDaPagina';
 
 import './AppAgroecomunicacao.css';
 
-import foto1 from '../../../assets/images/TecnologiasSociais/appAgro/foto1.png';
+import foto1 from '../../../assets/images/TecnologiasSociais/appAgro/logo_appAgro2.jpg';
 import icone from '../../../assets/images/TecnologiasSociais/appAgro/icone.png';
 
 const appAgroecomunicacao = () =>{
@@ -21,10 +21,10 @@ const appAgroecomunicacao = () =>{
                 <div className="row">
                     <div className="col-md-2"></div>
                     <div className="col-md-8 texto1">
-                    <p>O aplicativo surgiu da observação da vivência cotidiana dos assentados e assentadas produtores para escoarem a produção em canais alternativos durante as visitas de campo. A partir da reorganização destes canais em redes sociotécnicas de apoio outras formas de comercialização com a venda direta dos produtos foram desenhadas pelos agricultores e agricultoras do Assentamento Milton Santos, como a formação de grupos de consumo e pontos de venda em feiras orgânicas da Região Metropolitana de Campinas.</p>
-                    <p>Contudo, para que uma rede de consumo consciente das relações existentes entre os sistemas agroalimentares seja parte de um projeto de fortalecimento da agricultura familiar de forma consistente, sobretudo nos assentamentos de reforma agrária, é preciso dar visibilidade a estes territórios e sujeitos políticos.</p>
-                    <p>Desta forma, o objetivo do aplicativo é localizar estes territórios e sujeitos para o consumidor consciente da importância dos circuitos curtos e diretos de comercialização. Uma vez localizados os territórios, é possível encontrar dados sobre os pontos de venda em que os produtos são oferecidos, os arranjos comerciais existentes, periodicidade, dias e locais das entregas. Posteriormente, o aplicativo Agrocomunicação possibilitará a comunicação da produção pelos próprios agricultores e agricultoras orgânicos e agroecológicos destes territórios.</p>
-                    <p>O aplicativo foi desenvolvido pelos estudantes de Tecnologia em Análise e Desenvolvimento de Sistemas Letícia Franco Carvalho, Gabriel Bucalon, Winícius Andrade e Patrick Augusto Arashiro com orientação técnica dos professores Fábio Feliciano de Oliveira e Glauber da Rocha Balthazar.</p>
+                    <p>O aplicativo surgiu da vivência dos assentados e assentadas produtores para escoarem a produção em canais alternativos durante as visitas de campo. A partir da reorganização destes canais em redes sociotécnicas de apoio outras formas de comercialização foram desenhadas, como a formação de grupos de consumo e pontos de venda em feiras orgânicas da Região Metropolitana de Campinas.</p>
+                    <p>Contudo, para que uma rede de consumo consciente das relações existentes entre os sistemas agroalimentares seja parte de um projeto de fortalecimento da agricultura familiar, sobretudo nos assentamentos de reforma agrária, é preciso dar visibilidade a estes territórios e sujeitos políticos. Desta forma, o objetivo do aplicativo é localizá-los para o consumidor consciente da importância dos circuitos curtos e diretos de comercialização.</p>
+                    <p>Uma vez localizados os territórios, é possível encontrar dados sobre os pontos de venda em que os produtos são oferecidos, os arranjos comerciais existentes, periodicidade, dias e locais das entregas. Posteriormente, o aplicativo Agrocomunicação possibilitará a comunicação da produção pelos próprios agricultores e agricultoras orgânicos e agroecológicos destes territórios.</p>
+                    <p>O trabalho é parte do projeto da Chamada 21/2016 do CNPq e foi desenvolvido pelo profº Fábio Feliciano e pelas profªs Erika Batista e Zady Salazar, com o apoio de estudantes bolsistas de Tecnologia em Análise e Desenvolvimento de Sistemas durante 2018 e 2019. O aplicativo está disponível para sistema Android e o dowload liberado pela Play Store.</p>
                     </div>
                     <div className="col-md-2"></div>
                 </div>
@@ -34,7 +34,7 @@ const appAgroecomunicacao = () =>{
                     <div className="col-md-8">
                         <div className="card" style={{borderBottom: "4px solid #77B81E"}}> 
                             <div className="card-body fundo_cinza">
-                                <h4 className="card-title titulo3"><img src={icone} alt={"Icone_Download"}></img><a href=" https://drive.google.com/file/d/1brcHRQ0WlC06YIs_033OO55IN7wjaaCq/view" target="_blank"  rel="noopener noreferrer"> Clique aqui e baixe o APP</a></h4>
+                                <h4 className="card-title titulo3"><img src={icone} alt={"Icone_Download"}></img><a href=" https://apkpure.com/br/agrocomunica%C3%A7%C3%A3o-%E2%80%93-da-reforma-agr%C3%A1ria-at%C3%A9-voc%C3%AA/io.neaes.agrocomunicacao.starter" target="_blank"  rel="noopener noreferrer"> Clique aqui e baixe o APP</a></h4>
                             </div>
                         </div>
                     </div>
