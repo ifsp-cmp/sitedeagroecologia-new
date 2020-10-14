34b6bf173b81811eb94c8912f976091a7f107b78
João <joaovitordias92040@gmail.com>
2020-10-13 23:13:27 -0300

    Inserindo o SaibaMais

--- src/components/TecnologiasSociais/FichaAgroecologica/FichaAgroecologica.js
+++ src/components/TecnologiasSociais/FichaAgroecologica/FichaAgroecologica.js
@@ -4,9 +4,15 @@ import './FichaAgroecologica.css';
 
 import foto1 from '../../../assets/images/TecnologiasSociais/Fichas/foto1.png';
 import foto2 from '../../../assets/images/TecnologiasSociais/Fichas/foto2.png';
+import SaibaMais from '../../SaibaMais/SaibaMais';
 
-const fichaAgroecologica =()=>{
-    return(
+const fichaAgroecologica = () => {
+    const conteudo = <ul className="card-text">
+        <li><a href="https://drive.google.com/file/d/1CBqYvd8U8Qz_YyOESIj7F46HyrMW-7Vl/view?usp=sharing" target="_blank" rel="noopener noreferrer">Ficha Agroecológica - App Agrocomunicação</a></li>
+        <li><a href="https://drive.google.com/file/d/1f10ehIt0Y9CKrhkF-Y-2YLKU92QbrgK3/view?usp=sharing" target="_blank" rel="noopener noreferrer">Ficha Agroecológica - DataLuta Produção</a></li>
+    </ul>
+
+    return (
         <div>
             <TopoDaPagina menu="Tecnologias Sociais" submenu="Fichas Agroecológicas"></TopoDaPagina>
             <div className="container-fluid Ficha">
@@ -25,15 +31,15 @@ const fichaAgroecologica =()=>{
                     </div>
                     <div className="col-md-2"></div>
                 </div>
-                
-                <div className="row" style={{marginTop: '30px',marginBottom: '40px'}}>
+
+                <div className="row" style={{ marginTop: '30px', marginBottom: '40px' }}>
                     <div className="col-md-2"></div>
                     <div className="col-md-8">
                         <img src={foto2} width="100%" alt={"Fichas Agroecológicas"}></img>
                     </div>
                     <div className="col-md-2"></div>
                 </div>
-
+                <SaibaMais conteudo={conteudo} />
             </div>
         </div>
     )
