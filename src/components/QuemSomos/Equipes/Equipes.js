aaa85db7a8d614a8810b72a09d8c5718b7cb9b48
jooaodias <joaovitordias92040@gmail.com>
2022-05-15 20:47:45 -0300

    update members photos

--- src/components/QuemSomos/Equipes/Equipes.js
+++ src/components/QuemSomos/Equipes/Equipes.js
@@ -1,66 +1,91 @@
-import React from 'react';
-import TopoDaPagina from '../../TopoDaPagina/TopoDaPagina';
-import SaibaMais from '../../SaibaMais/SaibaMais';
-import './Equipes.css';
-import { membros } from '../../../data/membros';
-import CardEquipe from './CardEquipe';
-import foto1 from '../../../assets/images/QuemSomos/Introducao/quemsomos-03.png';
+import React from "react";
+import TopoDaPagina from "../../TopoDaPagina/TopoDaPagina";
+import SaibaMais from "../../SaibaMais/SaibaMais";
+import "./Equipes.css";
+import { membros } from "../../../data/membros";
+import CardEquipe from "./CardEquipe";
+import foto1 from "../../../assets/images/QuemSomos/Introducao/quemsomos-03.png";
 
 const equipes = () => {
-    const conteudo = <ul className="card-text">
-        <li><a href="http://dgp.cnpq.br/dgp/espelhogrupo/272964#recursosHumanos" target="_blank" rel="noopener noreferrer">Acesso ao Diretório do Grupo de Pesquisa NEAES no CNPq.</a></li>
-    </ul>;
+  const conteudo = (
+    <ul className="card-text">
+      <li>
+        <a
+          href="http://dgp.cnpq.br/dgp/espelhogrupo/272964#recursosHumanos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Acesso ao Diretório do Grupo de Pesquisa NEAES no CNPq.
+        </a>
+      </li>
+    </ul>
+  );
 
-    return (
-        <div>
-            <TopoDaPagina menu="Quem Somos" submenu="Equipes"></TopoDaPagina>
-            <div className="container-fluid Equipes">
-                <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8">
-                        <img src={foto1} width="100%" alt={"Equipe de alunos e professores do Neaes"}></img>
-                        <div className="texto1 ">
-                            <p><strong>COORDENAÇÃO GERAL:</strong></p>
-                            <p>Profª Erika Batista, Drª em Ciências Sociais e Pós-doutora em Relações Internacionais e Desenvolvimento. Contato: <a href="mailto:erika.batista@ifsp.edu.br" title="Envie um e-mail">erika.batista@ifsp.edu.br</a></p>
-                            <p>Profª  Mariana Saragiotto S. Alves, Drª Bióloga. Contato: <a href="mailto:mariana.alves@ifsp.edu.br" title="Envie um e-mail">mariana.alves@ifsp.edu.br</a></p>
-                        </div>
-                    </div>
-                    <div className="col-md-2"></div>
-                </div>
-                <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8">
-                        <p className="card-body planos">CONHEÇA NOSSA EQUIPE</p>
-                    </div>
-                </div>
-                <div className="container my-4">
-                    <div className="row justify-content-center">
-                        {membros.map(membro => (
-                            <CardEquipe
-                                key={membro.name}
-                                membro={membro}
-                            />
-                        ))
-
-                        }
-                    </div>
-                </div>
-
-                <div className="planosCinza">
-                    <div className="row">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            <p className="card-body planos">ACESSE NOSSO DIRETÓRIO NO CNPq</p>
-                        </div>
-                        <div className="col-md-2"></div>
-                    </div>
-                    <SaibaMais conteudo={conteudo}></SaibaMais>
-                </div>
+  return (
+    <div>
+      <TopoDaPagina menu="Quem Somos" submenu="Equipes"></TopoDaPagina>
+      <div className="container-fluid Equipes">
+        <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-8">
+            <img
+              src={foto1}
+              width="100%"
+              alt={"Equipe de alunos e professores do Neaes"}
+            ></img>
+            <div className="texto1 ">
+              <p>
+                <strong>COORDENAÇÃO GERAL:</strong>
+              </p>
+              <p>
+                Profª Erika Batista, Drª em Ciências Sociais. Contato:{" "}
+                <a
+                  href="mailto:erika.batista@ifsp.edu.br"
+                  title="Envie um e-mail"
+                >
+                  erika.batista@ifsp.edu.br
+                </a>
+              </p>
+              <p>
+                Profª Mariana Saragiotto S. Alves, Drª Bióloga. Contato:{" "}
+                <a
+                  href="mailto:mariana.alves@ifsp.edu.br"
+                  title="Envie um e-mail"
+                >
+                  mariana.alves@ifsp.edu.br
+                </a>
+              </p>
             </div>
-
-
+          </div>
+          <div className="col-md-2"></div>
         </div>
-    )
-}
+        <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-8">
+            <p className="card-body planos">CONHEÇA NOSSA EQUIPE</p>
+          </div>
+        </div>
+        <div className="container my-4">
+          <div className="row justify-content-center">
+            {membros.map((membro) => (
+              <CardEquipe key={membro.name} membro={membro} />
+            ))}
+          </div>
+        </div>
+
+        <div className="planosCinza">
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8">
+              <p className="card-body planos">ACESSE NOSSO DIRETÓRIO NO CNPq</p>
+            </div>
+            <div className="col-md-2"></div>
+          </div>
+          <SaibaMais conteudo={conteudo}></SaibaMais>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default equipes;
--- src/data/membros.js
+++ src/data/membros.js
@@ -1,214 +1,188 @@
-import andreFoto from '../assets/images/QuemSomos/Membros/andre.jpg'
-import arieleFoto from '../assets/images/QuemSomos/Membros/ariele.png'
-import carlaFoto from '../assets/images/QuemSomos/Membros/carla.jpeg'
-import daltamirFoto from '../assets/images/QuemSomos/Membros/daltamir.jpg'
-import diegoFoto from '../assets/images/QuemSomos/Membros/diego.jpg'
-import edsonFoto from '../assets/images/QuemSomos/Membros/edson.jpeg'
-import ellenFoto from '../assets/images/QuemSomos/Membros/ellen.jpg'
-import erikaFoto from '../assets/images/QuemSomos/Membros/erika.jpeg'
-import glauberFoto from '../assets/images/QuemSomos/Membros/glauber.jpg'
-import heriveltoFoto from '../assets/images/QuemSomos/Membros/herivelto.jpg'
-import jeanFoto from '../assets/images/QuemSomos/Membros/jean.jpeg'
-import joaoFoto from '../assets/images/QuemSomos/Membros/joao.jpeg'
-import jorgeFoto from '../assets/images/QuemSomos/Membros/jorge.jpeg'
-import leonardoFoto from '../assets/images/QuemSomos/Membros/leonardo.jpeg'
-import luizRFoto from '../assets/images/QuemSomos/Membros/luizR.jpeg'
-import manuFoto from '../assets/images/QuemSomos/Membros/manu.jpeg'
-import marcioFoto from '../assets/images/QuemSomos/Membros/marcio.jpg'
-import marianaFoto from '../assets/images/QuemSomos/Membros/mariana.jpg'
-import marianeFoto from '../assets/images/QuemSomos/Membros/mariane.jpeg'
-import mayFoto from '../assets/images/QuemSomos/Membros/may.jpeg'
-import nathanFoto from '../assets/images/QuemSomos/Membros/nathan.jpeg'
-import rodolfoFoto from '../assets/images/QuemSomos/Membros/rodolfo.jpeg'
-import samuelFoto from '../assets/images/QuemSomos/Membros/samuel.jpeg'
-import scheylaFoto from '../assets/images/QuemSomos/Membros/scheyla.jpeg'
-
+import andreFoto from "../assets/images/QuemSomos/Membros/andre.jpg";
+import arieleFoto from "../assets/images/QuemSomos/Membros/ariele.png";
+import brunoFoto from "../assets/images/QuemSomos/Membros/bruno.jpeg";
+import carlaFoto from "../assets/images/QuemSomos/Membros/carla.jpeg";
+import daltamirFoto from "../assets/images/QuemSomos/Membros/daltamir.jpg";
+import diegoFoto from "../assets/images/QuemSomos/Membros/diego.jpg";
+import edsonFoto from "../assets/images/QuemSomos/Membros/edson.jpeg";
+import erikaFoto from "../assets/images/QuemSomos/Membros/erika.jpeg";
+import heriveltoFoto from "../assets/images/QuemSomos/Membros/herivelto.jpg";
+import joaoFoto from "../assets/images/QuemSomos/Membros/joao.jpg";
+import joiceFoto from "../assets/images/QuemSomos/Membros/joice.jpeg";
+import jorgeFoto from "../assets/images/QuemSomos/Membros/jorge.jpeg";
+import kaioFoto from "../assets/images/QuemSomos/Membros/kaio.jpeg";
+import luizRFoto from "../assets/images/QuemSomos/Membros/luizR.jpeg";
+import manuFoto from "../assets/images/QuemSomos/Membros/manu.jpeg";
+import marcioFoto from "../assets/images/QuemSomos/Membros/marcio.jpg";
+import marianaFoto from "../assets/images/QuemSomos/Membros/mariana.jpg";
+import nathanFoto from "../assets/images/QuemSomos/Membros/nathan.jpeg";
+import rodolfoFoto from "../assets/images/QuemSomos/Membros/rodolfo.jpeg";
+import samuelFoto from "../assets/images/QuemSomos/Membros/samuel.jpeg";
+import scheylaFoto from "../assets/images/QuemSomos/Membros/scheyla.jpeg";
+import zainneFoto from "../assets/images/QuemSomos/Membros/zainne.jpeg";
 
 export const membros = [
-    {
-        name: 'André Bordignon',
-        photo: andreFoto,
-        role: 'Professor / Pesquisador',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2'
-    },
-    {
-        name: 'Ariele Contrigiani',
-        photo: arieleFoto,
-        role: 'Técnica',
-        // research2: 'Linha 2',
-        // researchLink2: '/quemsomos#linha2'
-    },
-    { 
-        name: 'Carla Vanessa',
-        photo: carlaFoto,
-        role: 'Nutricionista',
-        // research2: 'Linha 2',
-        // researchLink2: '/quemsomos#linha2'
-    },
-    {
-        name: 'Daltamir Maia',
-        photo: daltamirFoto,
-        role: 'Professor / Pesquisador',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    },
-    {
-        name: 'Diego Figueira',
-        photo: diegoFoto,
-        role: 'Professor / Pesquisador',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Edson Duarte',
-        photo: edsonFoto,
-        role: 'Professor / Pesquisador',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3',
-    },
-    {
-        name: 'Éllen Correa',
-        photo: ellenFoto,
-        role: 'Professora / Pesquisadora',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'Erika Batista',
-        photo: erikaFoto,
-        role: 'Professora / Pesquisadora',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    },
-    {
-        name: 'Glauber Balthazar',
-        photo: glauberFoto,
-        role: 'Professor / Pesquisador',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-        
-    },
-    {
-        name: 'Herivelto Rocha',
-        photo: heriveltoFoto,
-        role: 'Professor / Pesquisador',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-        
-    },
-    {
-        name: 'Jean Douglas ',
-        photo: jeanFoto,
-        role: 'Professor / Pesquisador',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'João Aleixo',
-        photo: joaoFoto,
-        role: 'Aluno',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Jorge Henrique ',
-        photo: jorgeFoto,
-        role: 'Técnico Educador Social',
-        // research2: 'Linha 2',
-        // researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Leonardo Cunha',
-        photo: leonardoFoto,
-        role: 'Aluno',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Luiz Roberto',
-        photo: luizRFoto,
-        role: 'Aluno',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Manuela Gracie',
-        photo: manuFoto,
-        role: 'Aluna',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'Mariana Saragiotto',
-        photo: marianaFoto,
-        role: 'Professora / Pesquisadora',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    },
-    {
-        name: 'Mariane Freitas',
-        photo: marianeFoto,
-        role: 'Aluna',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'Marcio Miranda',
-        photo: marcioFoto,
-        role: 'Professor / Pesquisador',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    },
-    {
-        name: 'Mayara Peres',
-        photo: mayFoto,
-        role: 'Pesquisadora',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'Nathan Silva',
-        photo: nathanFoto,
-        role: 'Aluno',
-        research2: 'Linha 2',
-        researchLink2: '/quemsomos#linha2',
-    },
-    {
-        name: 'Rodolfo Medina',
-        photo: rodolfoFoto,
-        role: 'Pesquisador',
-        research1: 'Linha 1',
-        researchLink1: '/quemsomos#linha1',
-    },
-    {
-        name: 'Samuel Dias',
-        photo: samuelFoto,
-        role: 'Aluno',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    },
-    {
-        name: 'Scheyla Saori',
-        photo: scheylaFoto,
-        role: 'Técnica Agrônoma',
-        research3: 'Linha 3',
-        researchLink3: '/quemsomos#linha3'
-    }
+  {
+    name: "André Bordignon",
+    photo: andreFoto,
+    role: "Professor / Pesquisador",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Ariele Contrigiani",
+    photo: arieleFoto,
+    role: "Técnica",
+  },
+  {
+    name: "Bruno Povliuk",
+    photo: brunoFoto,
+    role: "Aluno",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Carla Vanessa",
+    photo: carlaFoto,
+    role: "Nutricionista",
+  },
+  {
+    name: "Daltamir Maia",
+    photo: daltamirFoto,
+    role: "Professor / Pesquisador",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Diego Figueira",
+    photo: diegoFoto,
+    role: "Professor / Pesquisador",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Edson Duarte",
+    photo: edsonFoto,
+    role: "Professor / Pesquisador",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Erika Batista",
+    photo: erikaFoto,
+    role: "Professora / Pesquisadora",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Herivelto Rocha",
+    photo: heriveltoFoto,
+    role: "Professor / Pesquisador",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "João Aleixo",
+    photo: joaoFoto,
+    role: "Técnico",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Joice Mendes",
+    photo: joiceFoto,
+    role: "Professora",
+    research2: "Linha 2",
+  },
+  {
+    name: "Jorge Henrique",
+    photo: jorgeFoto,
+    role: "Técnico Educador Social",
+    // research2: 'Linha 2',
+    // researchLink2: '/quemsomos#linha2',
+  },
+  {
+    name: "Kaio Rodrigues",
+    photo: kaioFoto,
+    role: "Aluno",
+    research2: "Linha 2",
+  },
+  {
+    name: "Luiz Roberto",
+    photo: luizRFoto,
+    role: "Aluno",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Manuela Gracie",
+    photo: manuFoto,
+    role: "Aluna",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+  },
+  {
+    name: "Mariana Saragiotto",
+    photo: marianaFoto,
+    role: "Professora / Pesquisadora",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
 
-
-]
+  {
+    name: "Marcio Miranda",
+    photo: marcioFoto,
+    role: "Professor / Pesquisador",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Nathan Silva",
+    photo: nathanFoto,
+    role: "Aluno",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha2",
+  },
+  {
+    name: "Rodolfo Medina",
+    photo: rodolfoFoto,
+    role: "Pesquisador",
+    research1: "Linha 1",
+    researchLink1: "/quemsomos#linha1",
+  },
+  {
+    name: "Samuel Dias",
+    photo: samuelFoto,
+    role: "Aluno",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Scheyla Saori",
+    photo: scheylaFoto,
+    role: "Técnica Agrônoma",
+    research3: "Linha 3",
+    researchLink3: "/quemsomos#linha3",
+  },
+  {
+    name: "Zainne Soeiro Saori",
+    photo: zainneFoto,
+    role: "Aluna",
+    research2: "Linha 2",
+    researchLink2: "/quemsomos#linha3",
+  },
+];
